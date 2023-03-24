95c0283ef6bf9a8568883b2144adb44dbc3a7232
pragith98 <lakshanxp@hotmail.com>
2023-03-25 01:50:23 +0530

    change home directory name as "home-page"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './views/home/home.component';
+import { HomeComponent } from './views/home-page/home.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductComponent } from './components/product/product.component';
 
